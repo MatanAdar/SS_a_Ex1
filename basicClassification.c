81f36685cad09c74c50be1805d8d06f7bdbad5e9
MatanAdar <matan.adar@msmail.ariel.ac.il>
2022-11-27 09:12:33 +0200

    fix

--- basicClassification.c
+++ basicClassification.c
@@ -1,11 +1,13 @@
 #include "NumClass.h"
 #include <math.h>
 
-int isPrime(int n)
+int isPrime(int num)
 {
-    for (int i = 2; i <= sqrt(n) ; i++)
+    if(num==0)
+        return 0;
+    for (int i = 2; i <= sqrt(num) ; i++)
     {
-        if(n%i==0)
+        if(num%i==0)
         {
             return 0;
         }
@@ -22,10 +24,13 @@ int factorial(int x)
     return x*factorial(x-1);
 }
 
-int isStrong(int y)
+int isStrong(int num)
 {
+    if(num==0){
+        return 0;
+    }
     int sum=0;
-    int number=y;
+    int number=num;
 
     while(number>0)
     {
@@ -34,7 +39,7 @@ int isStrong(int y)
         number=number/10;
     }
 
-    if(sum==y)
+    if(sum==num)
     {
         return 1;
     }
